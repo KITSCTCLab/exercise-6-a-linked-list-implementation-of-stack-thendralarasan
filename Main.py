e2e94d2b31ea946b3504bddcdab40bde6ddd1a03
thendralarasan <110447168+thendralarasan@users.noreply.github.com>
2022-09-13 11:00:23 +0530

    second commit

--- Main.py
+++ Main.py
@@ -3,23 +3,70 @@ class Node:
     self.data = data
     self.next = None
 
+class Stack:
+  def __init__(self):
+    self.head = None
+
+  def push(self, data) -> None:
+    newnode = Node(data)
+    newnode.next = self.head
+    self.head = newnode
+
+  def pop(self) -> None:
+    if(self.head!=None):
+      temp = self.head
+      self.head = temp.next
+  
+  def status(self):
+    if(self.head  != None):
+      temp = self.head
+      while temp.next!= None:
+        print(temp.data, end = "")
+        print("=>", end = "")
+        temp = temp.next
+      print(temp.data, end = "=>")  
+    print("None")
+
+# Do not change the following code
+stack = Stack()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "push":
+    stack.push(int(data[i]))
+  elif operations[i] == "pop":
+    stack.pop()
+stack.status()class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
 
 class Stack:
   def __init__(self):
     self.head = None
 
   def push(self, data) -> None:
-    # Write your code here
+    newnode = Node(data)
+    newnode.next = self.head
+    self.head = newnode
 
   def pop(self) -> None:
-    # Write your code here
-
+    if(self.head!=None):
+      temp = self.head
+      self.head = temp.next
+  
   def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    # Write your code here  
-
+    if(self.head  != None):
+      temp = self.head
+      while temp.next!= None:
+        print(temp.data, end = "")
+        print("=>", end = "")
+        temp = temp.next
+      print(temp.data, end = "=>")  
+    print("None")
 
 # Do not change the following code
 stack = Stack()
